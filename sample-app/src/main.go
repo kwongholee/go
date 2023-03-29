fce9644fd71db8dd97e3e39597e5a301443f0618
kwongholee <ghlee9883@gmail.com>
2023-03-29 21:13:21 +0900

    3.29 고 언어 배우는중

--- sample-app/src/main.go
+++ sample-app/src/main.go
@@ -5,7 +5,19 @@ import "fmt"
 func main() {
 	var a int = 1
 	const b int = 32
-	var c float32 = 3.14
+	var c, d, e float32 = 3.14, 31.4, 5.9
+	const (
+		f = iota // 0
+		g        // 1
+		h        // 2
+	)
 
-	fmt.Println(a, b, c)
+	rawLiteral := `아리랑\n아리랑`
+	interLiteral := "아리랑\n아리랑"
+
+	fmt.Println(a, b, c, d, e, f, g, h)
+	fmt.Println()
+	fmt.Println(rawLiteral)
+	fmt.Println()
+	fmt.Println(interLiteral)
 }
